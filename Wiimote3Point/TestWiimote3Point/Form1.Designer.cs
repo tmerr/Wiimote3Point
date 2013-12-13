9c09d0d50fd80c56b23b75ddeeecc432016755bf
Trevor Merrifield <trevorm42@gmail.com>
2013-12-13 06:31:29 -0500

    Add test project

---
+++ Wiimote3Point/TestWiimote3Point/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TestWiimote3Point
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(12, 9);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(44, 13);
+            this.lblPosition.TabIndex = 0;
+            this.lblPosition.Text = "Position";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblPosition);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPosition;
+    }
+}
+
---
+++ Wiimote3Point/TestWiimote3Point/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Wiimote3Point;
+
+namespace TestWiimote3Point
+{
+    public partial class Form1 : Form
+    {
+        private Wiimote3P wiimote;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SensorTriangle s = new SensorTriangle(20, 30);
+            wiimote = new Wiimote3P(s);
+            wiimote.Wiimote3PChanged += wiimote_Wiimote3PChanged;
+            wiimote.Connect();
+        }
+
+        private void wiimote_Wiimote3PChanged(object sender, Wiimote3PChangedEventArgs args)
+        {
+            lblPosition.Invoke(new Action(() =>
+            {
+                lblPosition.Text = String.Format("{0}\n{1}\n{2}", args.positionOrientation.X, args.positionOrientation.Y, args.positionOrientation.Z);
+            }));
+        }
+    }
+}
